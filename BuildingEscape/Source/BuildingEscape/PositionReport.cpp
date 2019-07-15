ed7bd1ef4165b14f6280481cfae13aa2f751cbd6
Samuel Blake Rose <sambrose0712@gmail.com>
2019-07-15 16:21:22 -0500

    Explored Object Name accessors

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -9,7 +10,8 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
+    
+    
 	// ...
 }
 
@@ -20,6 +22,9 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+    FString ObjectName = GetOwner()->GetName();
+    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *ObjectName);
+
 	
 }
 
